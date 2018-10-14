6980081336ba68792ffd5c5bbdf138b45804c7cd
Ryan Whittingham <whittingham777@gmail.com>
2018-10-13 23:17:11 -0700

    Working command line input

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 
 int main (void) {
-
-///
-
+	
+	char buffer[120];
+	int stickCount;
+	
+	printf("Welcome to the Stick Game!\n");
+	printf("How many sticks would you like to play with?");
+	
+	fgets(buffer, 120, stdin);
+	sscanf(buffer, "%d", &stickCount);
+	
+	printf("%i\n", stickCount);
+	
 	return 0;
 }
