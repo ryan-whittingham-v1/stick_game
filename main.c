c62c00105b79e9b055e1363b8db588d3d951ad77
Ryan Whittingham <whittingham777@gmail.com>
2018-10-14 00:24:45 -0700

    Added prompt for stick count if no command line argument is given.

--- main.c
+++ main.c
@@ -1,17 +1,52 @@
 #include<stdio.h>
 
-int main (void) {
+int main (int argc, char** argv) {
 	
 	char buffer[120];
 	int stickCount;
+	int intCheck;
+	//sscanf(argv[1], "%i", &stickCount);
+
+	//printf("Welcome to the Stick Game!\n");
 	
-	printf("Welcome to the Stick Game!\n");
-	printf("How many sticks would you like to play with?");
+	//printf("How many sticks would you like to play with?");
 	
-	fgets(buffer, 120, stdin);
-	sscanf(buffer, "%d", &stickCount);
+	//fgets(buffer, 120, stdin);
+	//sscanf(buffer, "%d", &stickCount);
+
 	
-	printf("%i\n", stickCount);
+	if (argc == 1){ //No command line argument
+		printf("How many sticks would you like to play with?\n");
+		fgets(buffer, 120, stdin);
+		intCheck = sscanf(buffer, "%d", &stickCount);
+		if (intCheck != 1){
+			printf("That is not a number.\n");
+			return 0;
+		}
+	}
+
+
+	else if(argc != 2){
+		printf("Invalid number of arguments.\n");
+		return 0;
+	}
+
+	if(argc == 2){	
+		intCheck = sscanf(argv[1], "%i", &stickCount);
+		if (intCheck != 1){
+			printf("That is not a number.\n");
+			return 0;
+		}
+	}	
 	
+	printf("stickCount equals %i\n", stickCount);
+
+	if (stickCount < 10){
+		printf("That is too low of a number.\n");
+		return 0;	
+	}
+
+	printf("Okay. Let's play with %i sticks.\n", stickCount);
+
 	return 0;
 }
